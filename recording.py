40a49415840a16b9a1586384fc83904e6078ff4e
SodaCris <18463922396@163.com>
2022-01-07 11:19:12 +0800

    fix preview

--- recording.py
+++ recording.py
@@ -18,12 +18,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         # Create an empty vlc media player
         self.mediaplayer = self.instance.media_player_new()
-        
-        # In this widget, the video will be drawn
-        if platform.system() == "Darwin": # for MacOS
-            self.main_frame = QtWidgets.QMacCocoaViewContainer(0)
-        else:
-            self.main_frame = QtWidgets.QFrame()
 
         self.palette = self.main_frame.palette()
         self.palette.setColor(QtGui.QPalette.Window, QtGui.QColor(0, 0, 0))
@@ -34,32 +28,32 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def play(self):
         cam_addr = "rtsp://admin:hk123456@{}:554/Streaming/Channels/101".format(
-            self.cam_addr_edit.toPlainText().strip()
+            self.cam_addr_edit.text().strip()
         )
         # 192.168.104.72
         self.media = self.instance.media_new(cam_addr)
-        self.mediaplayer.set_media(self.media)
         self.media.parse()
+        self.mediaplayer.set_media(self.media)
         # The media player has to be 'connected' to the QFrame (otherwise the
         # video would be displayed in it's own window). This is platform
         # specific, so we must give the ID of the QFrame (or similar object) to
         # vlc. Different platforms have different functions for this
-        if platform.system() == "Linux": # for Linux using the X Server
+        if platform.system() == "Linux":  # for Linux using the X Server
             self.mediaplayer.set_xwindow(int(self.main_frame.winId()))
-        elif platform.system() == "Windows": # for Windows
+        elif platform.system() == "Windows":  # for Windows
             self.mediaplayer.set_hwnd(int(self.main_frame.winId()))
-        elif platform.system() == "Darwin": # for MacOS
+        elif platform.system() == "Darwin":  # for MacOS
             self.mediaplayer.set_nsobject(int(self.main_frame.winId()))
         self.mediaplayer.play()
-    
+
 
 def main():
-    """Entry point for our simple vlc player
-    """
+    """Entry point for our simple vlc player"""
     app = QtWidgets.QApplication(sys.argv)
     player = MainWindow()
     player.show()
     sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
